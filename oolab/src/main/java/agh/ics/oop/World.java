e1b9bc74f2ab21bf9bf0d7e22bf10390efe90ffb
Agnieszka <agadabek.sa@gmail.com>
2024-10-27 15:02:44 +0100

    lab_2 comm

--- oolab/src/main/java/agh/ics/oop/World.java
+++ oolab/src/main/java/agh/ics/oop/World.java
@@ -1,15 +1,41 @@
 package agh.ics.oop;
 
 import agh.ics.oop.model.MoveDirection;
+import agh.ics.oop.model.MapDirection;
+import agh.ics.oop.model.Vector2d;
 
 public class World {
     public static void main(String[] args) {
-        System.out.println("Start");
+        //lab2
+        Vector2d startPoint = new Vector2d(3, 4);
+        System.out.println("Start Point: " + startPoint);
+
+        Vector2d endPoint = new Vector2d(-1, 3);
+        System.out.println("End Point: " + endPoint);
+
+        Vector2d resultVector = startPoint.add(endPoint);
+        System.out.println("Result of addition: " + resultVector);
+
+        MapDirection currentDirection = MapDirection.WEST;
+        System.out.println("Current Direction: " + currentDirection);
+
+        MapDirection nextDirection = currentDirection.next();
+        System.out.println("Next Direction: " + nextDirection);
+
+        MapDirection previousDirection = currentDirection.previous();
+        System.out.println("Previous Direction: " + previousDirection);
+
+        Vector2d unitVector = currentDirection.toUnitVector();
+        System.out.println("Unit Vector of Current Direction: " + unitVector);
+
+
+        //lab1
+        /*System.out.println("Start");
 
         MoveDirection[] directions = OptionsParser.parse(args);
         run(directions);
 
-        System.out.println("Stop");
+        System.out.println("Stop");*/
     }
 
     public static void run(MoveDirection[] directions) {
